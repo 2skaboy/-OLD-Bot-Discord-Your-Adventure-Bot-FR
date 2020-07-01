8ee50bc37f5a4cb5e140e9ad24dfd207f5499e62
Gymp_MC <64908200+Gympmc@users.noreply.github.com>
2020-07-01 12:13:21 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-const Discord = require ('discord.js')
-const bot = new Discord.Client()
-let embed = new Discord.RichEmbed()
-const fs = require ('fs')
-bot.commands = new Discord.Collection()
+const Discord = require ('discord.js');
+const bot = new Discord.Client();
+let embed = new Discord.MessageEmbed();
+const fs = require ('fs');
+bot.commands = new Discord.Collection();
 
 //instance
 bot.on('ready', function () {
     console.log("/////////////////////////////");
     console.log("[!] Connexion effectué [!]");
     console.log("/////////////////////////////");
-    bot.user.setActivity('*help');
+    bot.user.setActivity('--help');
 
 
 })
-        bot.login("")
+        bot.login("my token :)")
 
-        bot.on('message', async message => {
+        bot.on('message', message => {
+
+            const info_embed = new Discord.MessageEmbed()
+            .setColor('13ff00')
+            .setThumbnail('https://cdn.discordapp.com/attachments/727222299551465554/727222355914391632/3094891.png').setTitle('Aide :')
+            .setDescription('Je suis un Bot qui fait vivre une histoire aux utilisateur du serveur') 
+            .addField('Mes commandes sont :')
+            .addField('```--jungle```')
+            .addField('```--military```')
+            .addField('Ces commandes sont pour commencer différentes aventures, bon jeu !')
+            .setTimestamp()
+            .setFooter('Développé par Gymp_MC (Gymp_#4272)')
+        
+
             if(message.content === "--help"){
-                let info_embed = new Discord.RichEmbed()
-                .setColor("13ff00")
-                .setThumbnail("")
-                .setTitle("Aide :")
-                .setDescription("") 
-                .setTimestamp()
-                .setFooter("Développé par Gymp_MC (Gymp_#4272)")
-                .addField("")
                 message.channel.send(info_embed)
-
-            } 
-        })
+                } 
+            })
